c79d007dee95d63364a8d2380e7b056df209edec
Daniel Davenport <pinerd10@gmail.com>
2020-01-09 10:07:24 -0700

    Created Part 2

---
+++ CST 210 In Class Activity/Source.cpp
@@ -0,0 +1,14 @@
+//Daniel Davenport
+//This is my own work
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	float x; //creates variable for input
+	cout << "Please inset number of pounds to be converted into kilograms \n"; //asks for input
+	cin >> x; //gathers input
+	cout << x << " pounds is " << x * 0.454 << " kilograms";
+
+	return 0;
+}
